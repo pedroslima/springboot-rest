d2916cd2f48282a26cf3efbbfe8103ed9525b9e5
Pedro Lima <pedro.lima@ncr.com>
2020-01-21 20:51:39 -0300

    Representacao Restful

--- src/main/java/com/plima/payroll/EmployeeController.java
+++ src/main/java/com/plima/payroll/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.plima.payroll;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -16,8 +22,15 @@ public class EmployeeController {
   //Agregate root
 
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(employee -> new EntityModel<Employee>(employee,
+            linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel()))
+        .collect(Collectors.toList());
+
+    return new CollectionModel<>(employees,
+        linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
 
   @PostMapping("/employees")
@@ -27,9 +40,14 @@ public class EmployeeController {
 
   // Single item
   @GetMapping("/employees/{id}")
-  Employee one(@PathVariable Long id) {
-    return repository.findById(id)
+  EntityModel<Employee> one(@PathVariable Long id) {
+
+    Employee employee = repository.findById(id)
         .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+    return new EntityModel<>(employee,
+        linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
   }
 
   @PutMapping("/employees/{id}")
